5cd558c1dc1d9a0261d451c4c71969cdd803f150
mxxt1 <matt@dustcloudlabs.com>
2019-11-16 09:27:14 -0500

    add production/postgres  boilerplating in knex.js for future heroku deployment, added npm package 'pg' to accomodate sqlite --> postgres deployment

--- knexfile.js
+++ knexfile.js
@@ -37,18 +37,13 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
