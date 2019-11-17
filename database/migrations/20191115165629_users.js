840d860858a00c45ea99e7b48cac3feb67e9c4ab
Mark Gowen <mark.gowen78@gmail.com>
2019-11-16 16:59:03 -0800

    created db migrations
    created tbls for:
    users
    hobbies
    messages
    conversations
    user_match
    user_hobbies

--- database/migrations/20191115165629_users.js
+++ database/migrations/20191115165629_users.js
@@ -1,8 +1,116 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments("user_id");
+
+      tbl.integer("match_id").notNullable().unique.increments;
+      tbl.integer("sender_id").notNullable().unique.increments;
+      tbl.integer("recipient_id").notNullable().unique.increments;
+      tbl.string("email", 255).notNullable().unique;
+      tbl.string("password", 255).notNullable();
+      tbl.string("first_name", 255).notNullable();
+      tbl.string("last_name", 255).notNullable();
+      tbl.integer("age").notNullable();
+      tbl.string("gender", 64);
+      tbl.string("city", 128);
+      tbl.string("state", 128);
+      tbl.integer("zipcode", 9).notNullable();
+    })
+    .createTable("hobbies", tbl => {
+      tbl.increments("hobbies_id");
+
+      tbl.string("hobbies", 255).notNullable();
+    })
+    .createTable("messages", tbl => {
+      tbl.increments("message_id");
+
+      tbl.string("message_body", 255);
+      tbl.datetime("time").notNullable();
+    })
+    .createTable("conversations", tbl => {
+      tbl.increments();
+
+      tbl
+        .integer("sender_id")
+        .notNullable()
+        .unsigned()
+        .references("sender_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("recipient_id")
+        .notNullable()
+        .unsigned()
+        .references("recipient_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("message_id")
+        .notNullable()
+        .unsigned()
+        .references("message_id")
+        .inTable("messages")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("user_match", tbl => {
+      tbl.increments();
+
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("match_id")
+        .notNullable()
+        .unsigned()
+        .references("match_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .boolean("match")
+        .notNullable()
+        .defaultTo("false");
+      tbl
+        .boolean("interested")
+        .notNullable()
+        .defaultTo("false");
+    })
+    .createTable("user_hobbies", tbl => {
+      tbl.increments();
+
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("hobbies_id")
+        .notNullable()
+        .unsigned()
+        .references("hobbies_id")
+        .inTable("hobbies")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("user_hobbies")
+    .dropTableIfExists("user_match")
+    .dropTableIfExists("conversations")
+    .dropTableIfExists("messages")
+    .dropTableIfExists("hobbies")
+    .dropTableIfExists("users");
 };
