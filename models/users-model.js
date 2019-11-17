912f1a8ad6bed78cdfdd5493e4aff06f70af550b
Mark Gowen <mark.gowen78@gmail.com>
2019-11-17 10:48:19 -0800

    created users-model
    find,
    findBy,
    findById,
    insert,
    update,
    remove

---
+++ models/users-model.js
@@ -0,0 +1,44 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  insert,
+  update,
+  remove
+};
+
+function find() {
+  return db("users");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function findById(user_id) {
+  return db("users")
+    .where({ user_id })
+    .first();
+}
+
+function insert(user) {
+  return db("users")
+    .insert(user)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function update(user_id, changes) {
+  return db("users")
+    .where({ user_id })
+    .update(changes);
+}
+
+function remove(user_id) {
+  return db("users")
+    .where("user_id", user_id)
+    .del();
+}
