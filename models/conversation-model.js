84e21ad28b0e3078acb852607d472082ddeae68b
mxxt1 <matt@dustcloudlabs.com>
2019-11-18 18:45:47 -0500

    Updated findConvoById
    Co-Authored-By: Mark Gowen <markgowen@users.noreply.github.com>

--- models/conversation-model.js
+++ models/conversation-model.js
@@ -8,14 +8,17 @@ module.exports = {
 function findConvoById({ user_id }) {
   const id = user_id;
 
-  return db
-    .select("users.*")
-    .from("conversations")
-    .join("users", "users.user_id")
-    .where({
-      person_1: id,
-      person_2: id
-    });
+  return db.raw(`SELECT * from conversations JOIN users ON (users.user_id = conversations.person_1 OR users.user_id = conversations.person_2) AND NOT users.user_id = ${id} WHERE conversations.person_1 = ${id} OR conversations.person_2 = ${id}`)
+
+
+  // return db
+  //   .select("users.*")
+  //   .from("conversations")
+  //   .join("users", "users.user_id")
+  //   .where({
+  //     person_1: id,
+  //     person_2: id
+  //   });
 }
 
 function insertConvo(convo) {
