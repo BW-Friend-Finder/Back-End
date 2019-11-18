c0b352e934f4f0f44d4c28ff6dcd248f24d6a062
mxxt1 <matt@dustcloudlabs.com>
2019-11-18 15:36:12 -0500

    Update match-router.js
    Co-Authored-By: Mark Gowen <markgowen@users.noreply.github.com>

--- routes/match-router.js
+++ routes/match-router.js
@@ -6,7 +6,14 @@ const {authorize, validate} = require('../middleware/authenticationMW');
 const match = require('../models/match-model.js');
 
 
+router.get('/:id', (req, res) => {
+    const matchId = req.params.id;
 
+    match.findByMatchId(matchId)
+    .then(match => {
+        
+    })
+})
 
 
 
