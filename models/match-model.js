c80c5976ba19fe7b6dbd2d72c5452e69b7751481
mxxt1 <matt@dustcloudlabs.com>
2019-11-18 15:35:17 -0500

    match router template, files for message router and model

--- models/match-model.js
+++ models/match-model.js
@@ -1,16 +1,17 @@
 const db = require("../configs/dbConfig");
 
 module.exports = {
-  findByMatchId,
   findByRequesterId,
   findByRequesteeId,
   insertMatch,
+  findMatched
 };
 
-function findByMatchId(id) {
+function findMatched() {
   return db
     .select("user_match.id", "user.user_id", "user_match.requester_id")
     .from("user_match")
+    .where()
     .join("users", "users.user_id", "user_match.requestee_id")
     .where("user_match.matched", true);
 }
@@ -18,13 +19,26 @@ function findByMatchId(id) {
 function findByRequesterId({ user_id }) {
     const id = user_id
     
-    return db('user_match').where({ requester_id = id }).first()
+    return db('user_match').where(requester_id = id);
 }
 
 function findByRequesteeId({ user_id }) {
     const id = user_id
 
-    return db('user_match').where({ requestee_id = id }).first()
+    return db('user_match').where(requestee_id = id);
+}
+
+function findMatches({ user_id }) {
+  const id = user_id
+
+  // return db('user_match').where({requestee_id = id || requester_id = id}).and(user_match.matched = 1);
+
+  return db('user_match').where(
+    {
+      requestee_id: id,
+      requester_id: id,
+      matched: 1
+    });
 }
 
 function insertMatch(match) {
---
+++ routes/match-router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router;
+
+
+const {authorize, validate} = require('../middleware/authenticationMW');
+
+const match = require('../models/match-model.js');
+
+
+
+
+
+
+
+
+module.exports = router; 
