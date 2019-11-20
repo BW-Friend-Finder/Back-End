c4f482fe65108d64bf3ffc9b0fbcea75f4403342
mxxt1 <matt@dustcloudlabs.com>
2019-11-19 21:28:00 -0500

    fixed findConvoById to return relevant data about a user's convo. Added getConvoMessages to get all messages associated with a conversation. Need to add: add a new message, delete a message, delete a convesation to model and routes.

--- routes/conversation-router.js
+++ routes/conversation-router.js
@@ -4,20 +4,24 @@ const { authorize } = require("../middleware/authenticationMW");
 
 const db = require("../models/conversation-model");
 
-//get specific convo by convo id
-router.get("/convo/:id", authorize, (req, res) => {
-  const id = req.body.id; //front end needs to send back the conversation id
 
-  db.findConvoById(id)
-    .then(convo => {
-      console.log(convo);
-      res.status(200).json(convo);
-    })
-    .catch(err => {
-      console.log("Error...", err);
-      res.status(500).json({ error: "Failed to retrieve messages" });
-    });
-});
+// /api/users/convo/
+
+
+//get specific convo by convo id
+// router.get("/convo/", authorize, (req, res) => {
+//   const id = req.body.id; //front end needs to send back the conversation id
+
+//   db.findConvoById(id)
+//     .then(convo => {
+//       console.log(convo);
+//       res.status(200).json(convo);
+//     })
+//     .catch(err => {
+//       console.log("Error...", err);
+//       res.status(500).json({ error: "Failed to retrieve messages" });
+//     });
+// });
 
 //get all of a users convo's by user id
 router.get("/convo/", authorize, (req, res) => {
@@ -35,8 +39,8 @@ router.get("/convo/", authorize, (req, res) => {
 });
 
 
-
-router.post("/convo/:id", authorize, (req, res) => {
+//what is the purpose of this?
+router.post("/convo/", authorize, (req, res) => {
   //frontend must send the conversations id. Will have access from the initial get all convos by user_id
   const chat = { 
     ...req.body, 
@@ -60,5 +64,32 @@ router.post("/convo/:id", authorize, (req, res) => {
       res.status(500).json({ error: "Failed to initialize chat" });
     });
 });
+
+
+//get all messages from a convo_id
+router.get('/messages', authorize, (req, res) => {
+  const convo_id = req.body.id;
+
+  db.getConvoMessages(convo_id)
+  .then(messages => {
+    console.log(messages);
+    res.status(200).json({message: 'successfully retrieved messages', messages});
+  })
+  .catch(error => {
+    res.status(500).json({error: error, message: `failed to retrive messages for this conversation`});
+  });
+});
+
+//post new message to a conversation
+
+
+//delete conversation
+
+
+//delete message
+
+
+
+
 
 module.exports = router;
