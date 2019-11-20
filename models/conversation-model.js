be4cee2750e61f74db05f1c647f1c6815ef4bde9
mxxt1 <matt@dustcloudlabs.com>
2019-11-19 21:31:10 -0500

    progress

--- models/conversation-model.js
+++ models/conversation-model.js
@@ -21,7 +21,7 @@ function findConvoById(user_id) {
   return db.raw(`SELECT conversations.id, users.first_name, users.last_name FROM conversations JOIN users ON (users.user_id = conversations.person_1 OR users.user_id = conversations.person_2) AND NOT users.user_id = ${id} WHERE conversations.person_1 = ${id} OR conversations.person_2 = ${id}`);
 }
 
-//add a new convo --> inserts a new convo (will need id for person_2. person_1 is user, pulled from token)
+//add a new convo --> inserts a new convo (will need id for person_2. person_1 is user, pulled from token) needs route
 function insertConvo(convo) {
   return db("conversations").insert(convo);
 }
